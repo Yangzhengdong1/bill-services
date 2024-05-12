3037b4e22c89921a7d3991dfb40a4ecf1ca66882
zdyang <yangzhengdongyang@gmail.com>
2024-05-12 19:46:34 +0800

    fix：修复小程序注册失败的问题

--- src/service/account.service.js
+++ src/service/account.service.js
@@ -46,17 +46,20 @@ class AccountService {
   async register(params) {
     try {
       const { openid, avatarUrl, nickname, username, password } = params;
-      let statement = "";
+      let statement = "", paramArray = [];
       if (username && password) {
         statement = `
           INSERT INTO users(openid, avatar_url, nickname, name, password) VALUES(?, ?, ?, ?, ?);
         `;
+        paramArray = [ openid, avatarUrl, nickname, username, password ];
       } else {
         statement = `
           INSERT INTO users(openid, avatar_url, nickname) VALUES(?, ?, ?);
         `;
+        paramArray = [ openid, avatarUrl, nickname ];
       }
-      const [ result ] = await connection.execute(statement, [ openid, avatarUrl, nickname, username, password ]);
+
+      const [ result ] = await connection.execute(statement, paramArray);
       return result;
     } catch (error) {
       console.log(error, "用户注册");
